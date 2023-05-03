c5e54a9c84a6af73127210605ab2d675a835350d
Pzzles <125101956+Pzzles@users.noreply.github.com>
2023-05-03 02:36:10 +0200

    Project Setup

---
+++ src/test/java/com/puleprojects/TenantManagementSystem/TenantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puleprojects.TenantManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
